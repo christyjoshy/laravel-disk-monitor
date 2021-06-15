f53125940588590c341e83cfeae0a31abbbcba9c
christyjoshy <christyjoshy@users.noreply.github.com>
2021-06-15 07:42:09 +0000

    Fix styling

--- tests/Feature/Commands/LaravelDiskMonitorCommandTest.php
+++ tests/Feature/Commands/LaravelDiskMonitorCommandTest.php
@@ -5,25 +5,24 @@ use Illuminate\Support\Facades\Storage;
 use Rubix\LaravelDiskMonitor\Models\DiskMonitorEntry;
 use Rubix\LaravelDiskMonitor\Tests\TestCase;
 
-class LaravelDiskMonitorCommandTest extends TestCase{
+class LaravelDiskMonitorCommandTest extends TestCase
+{
     public function setUp(): void
     {
         parent::setUp();
         Storage::fake('local');
     }
-        // @test
+
+    // @test
 
     public function it_will_record_zero_files_for_empty_disks()
     {
-        Storage::disk('local')->put('test.txt','test');
+        Storage::disk('local')->put('test.txt', 'test');
         $this->artisan(LaravelDiskMonitorCommand::class)
-            ->assertExitCode(0);    
+            ->assertExitCode(0);
         $this->assertCount(1, DiskMonitorEntry::get());
         
         $entry = DiskMonitorEntry::last();
         $this->assertEquals(1, $entry->file_count);
     }
-
 }
-
-?>
