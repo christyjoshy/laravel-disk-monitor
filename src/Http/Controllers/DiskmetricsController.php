4bab8f5eaae22e084920dbac16d62093cbb9dac5
christy <chisty.joshy@altd.in>
2021-06-16 12:55:39 +0530

    bug fix

--- src/Http/Controllers/DiskmetricsController.php
+++ src/Http/Controllers/DiskmetricsController.php
@@ -8,11 +8,7 @@ class DiskMetricsController{
 
     public function index()
     {
-        //return 'ok';
         $entries = DiskMonitorEntry::latest()->get();
-        // $entries->disk_name = 'local';
-        // $entries->file_count = 1;
-        // dd($entries);
 
         return view('disk-monitor::entries',compact('entries'));
     }
